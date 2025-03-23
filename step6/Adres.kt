8b1c7cbce0dfa87b36789adcac14a1a5efd00839
syw <degirmenseyit@gmail.com>
2025-03-24 01:31:31 +0300

    data class update

--- step6/Adres.kt
+++ step6/Adres.kt
@@ -1,4 +1,4 @@
 package com.calisma.step6
 
-class Adres(val mahalle: String ,val cadde: String ,val binaNo: Int ,val daireNo: Int) {
+data class Adres(val mahalle: String ,val cadde: String ,val binaNo: Int ,val daireNo: Int) {
 }
