9ac51119b440bc416bd1c93c755133098847d96f
Seyit Ali Degirmen <degirmenseyit@gmail.com>
2025-03-30 10:15:52 +0300

    repeat 1.2

--- Customer.kt
+++ Customer.kt
@@ -1,6 +1,6 @@
 package com.calisma.repeat1
 
-class Customer(val name : String , var balance : Double) {
+class Customer(val name : String , var balance : Double ,val kriptoHesabim : CryptoPayment , val krediKartim : CreditCardPayment , val payPalHesabim : PayPalPayment) {
 
     fun makePayment(paymentMethod : PaymentMethod ,amount : Double)
     {
@@ -8,8 +8,13 @@ class Customer(val name : String , var balance : Double) {
         {
             val topAmount = amount + amount/50
             if (balance > topAmount)
+            {
                 if (paymentMethod.processPayment(amount))
                     balance -= topAmount
+            }
+            else
+                println("Hesap bakiyeniz ${topAmount - balance}TL eksik")
+
         }
         else{
             if (balance > amount)
